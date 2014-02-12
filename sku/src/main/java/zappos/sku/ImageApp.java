ca5b6ce8a36d5dd6dba55d38d91c9787b416ba9b
judd.bourgeois <judd.bourgeois@gmail.com>
2014-02-11 17:56:29 -0800

    complete

--- sku/src/main/java/zappos/sku/ImageApp.java
+++ sku/src/main/java/zappos/sku/ImageApp.java
@@ -1,15 +1,29 @@
 package zappos.sku;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 
+import org.apache.commons.httpclient.Header;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 
 /**
  * Hello world!
@@ -17,6 +31,9 @@ import org.apache.log4j.Logger;
  */
 public class ImageApp {
     private static final Logger logger = Logger.getLogger(ImageApp.class);
+
+    private static final String PRODUCT_API_URL_BASE = "http://api.zappos.com/Product";
+    private static final String PRODUCT_API_KEY = "52ddafbe3ee659bad97fcce7c53592916a6bfd73";
 
     public static void main(String[] args) {
         // Check arguments
@@ -51,7 +68,7 @@ public class ImageApp {
                 if (StringUtils.isBlank(skuLine)) {
                     continue;
                 }
-                
+
                 // Trim white space before adding
                 skuList.add(StringUtils.strip(skuLine));
             }
@@ -71,11 +88,111 @@ public class ImageApp {
         fis = null;
 
         // Build URL
+        HttpClient client = new HttpClient();
+        NameValuePair queryStringParams[] = new NameValuePair[1];
+        NameValuePair apiKeyNVPair = new NameValuePair();
+        apiKeyNVPair.setName("key");
+        apiKeyNVPair.setValue(PRODUCT_API_KEY);
+        queryStringParams[0] = apiKeyNVPair;
 
-        // Get image URL from JSON response
+        // Iterate over SKU list
+        for (String sku : skuList) {
+            String url = PRODUCT_API_URL_BASE + "/" + sku;
+            HttpMethod method = new GetMethod(url);
+            method.setQueryString(queryStringParams);
 
-        // Retrieve image
+            try {
+                int status = client.executeMethod(method);
+                if (status != HttpStatus.SC_OK) {
+                    logger.error("Error retrieving SKU: " + sku + ": HTTP status: " + status);
+                } else {
+                    byte[] responseBody = method.getResponseBody();
+                    processResponseBody(sku, responseBody);
+                }
+            } catch (IOException e) {
+                logger.error("Error retrieving SKU: " + sku + ": " + e.getMessage());
+            } finally {
+                method.releaseConnection();
+            }
+        }
+    }
 
-        // Save image
+    protected static void processResponseBody(String sku, byte[] responseBody) {
+        // Define this once here, we will reuse it
+        Object o;
+
+        // Parse JSON response & create object
+        o = JSONValue.parse(new String(responseBody));
+        JSONObject json = (JSONObject) o;
+
+        // Build list of images
+        o = json.get("product");
+        JSONArray product = (JSONArray) o;
+
+        ArrayList<String> imageUrls = new ArrayList<String>();
+        for (int i = 0; i < product.size(); i++) {
+            o = product.get(i);
+            JSONObject entry = (JSONObject) o;
+            String defaultImageUrl = (String) entry.get("defaultImageUrl");
+
+            // Check for empty string
+            if (!StringUtils.isBlank(defaultImageUrl)) {
+                imageUrls.add(defaultImageUrl);
+            }
+            
+            logger.debug("sku/defaultImageUrl: " + sku + "/" + defaultImageUrl);
+        }
+
+        // Retrieve & Save images
+        for (String url : imageUrls) {
+            try {
+                retrieveAndSaveImage(sku, url);
+            } catch (Exception e) {
+                logger.warn("Could not save image for SKU: " + sku + ": " + e.getMessage());
+            }
+        }
+    }
+
+    protected static void retrieveAndSaveImage(String sku, String url) {
+        HttpClient client = new HttpClient();
+        HttpMethod method = new GetMethod(url);
+        int status;
+        byte[] imageData = null;
+        try {
+            status = client.executeMethod(method);
+            if (status != HttpStatus.SC_OK) {
+                logger.error("Error retrieving image for SKU at URL: " + sku + ": " + url + ": HTTP status: " + status);
+                return;
+            }
+            imageData = method.getResponseBody();
+            logger.debug("imageData: " + imageData.length);
+        } catch (IOException e) {
+            logger.error("Error retrieving image for SKU at URL: " + sku + ": " + url + ": " + e.getMessage());
+            return;
+        }
+
+        // Create images directory if necessary
+        File imagesDir = new File("images");
+        if (!imagesDir.exists()) {
+            logger.info("Creating images directory");
+            boolean result = imagesDir.mkdir();
+            if (!result) {
+                logger.error("Failed to create images directory");
+            }
+        }
+
+        // Save to disk
+        String baseName = FilenameUtils.getBaseName(url);
+        String extension = FilenameUtils.getExtension(url);
+        String fileName = "images" + File.separator + baseName + "." + extension;
+        FileOutputStream output;
+        try {
+            output = new FileOutputStream(new File(fileName));
+            IOUtils.write(imageData, output);
+        } catch (FileNotFoundException e) {
+            logger.error("Failed to save image: " + fileName);
+        } catch (IOException e) {
+            logger.error("Failed to save image: " + fileName);
+        }
     }
 }
